d55a4d07c4f261e612497c8b340be49f7b8717e2
Mubbashirkhan87 <mubbashirkhan8787505@gmail.com>
2025-01-24 02:11:26 -0800

    fist

---
+++ script.js
@@ -0,0 +1,52 @@
+//firstName & lastName
+var firstName = "mubashir";
+var secondName = "khan";
+
+var fullName = firstName + secondName;
+
+alert("mubbashir khan");
+
+// Add
+var num1 = 3;
+var num2 = 4;
+var num3 = num1 + num2;
+ 
+alert(3+4)
+//Sub
+var num1 = 5;
+var num2 = 3;
+var num3 = num1 - num2;
+ 
+alert(num1-num2)
+//Mult
+var num1 = 3;
+var num2 = 4;
+var num3 = num1 * num2;
+ 
+alert(num1*num2)
+//Divi
+var num1 = 8;
+var num2 = 2;
+var num3 = num1 /num2;
+ 
+alert(num1 / num2)
+// %
+var num1 = 14;
+var num2 = 2;
+var num3 = num1 % num2;
+ 
+alert(num1 % num2)
+
+// width and height
+var width = 14;
+var height = 17;
+
+var bothanswer = width * height;
+alert(width * height)
+
+// tempreature value 
+var temp = 2;
+var celius = 9;
+var nowtempreature =(Celsius * 9/5) + 32 ;
+alert(nowtempreature)
+
